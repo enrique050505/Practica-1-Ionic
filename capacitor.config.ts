5ba3d66a445aeeb0d3851c7a5064921d35f75d07
Rubenchus18 <rubengarci327@gmail.com>
2024-09-30 13:56:01 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'RubenM_EnriqueMA',
+  webDir: 'www'
+};
+
+export default config;
