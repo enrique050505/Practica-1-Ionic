b3c65a67b2dd68dd1a5f1e4156365fb9c2b6858c
enrique050505 <enriquemartinalbodo@gmail.com>
2024-10-04 20:30:22 +0200

    primera pagina finalizada

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -9,6 +9,23 @@ export class InicioPage implements OnInit {
 
   constructor() { }
 
+  //Iconos Botones Pagina Inicio
+  iconoBoton1 = "person-outline";
+  iconoBoton2 = "game-controller-outline";
+
+  //Iconos 
+  iconoTwitter ="logo-twitter";
+  iconoInstagram = "logo-instagram";
+  iconoFacebook = "logo-facebook"; 
+  iconowhatsapp = "logo-whatsapp"
+  iconoCasa = "home-outline";
+
+  //Enlaces redes Sociales
+  enlaceInstagram = "https://www.instagram.com";
+  enlaceTwitter = "https://x.com/home?lang=es";
+  enlaceFacebook = "https://www.facebook.com/?locale=es_ES";
+  enlaceWhatsapp ="https://web.whatsapp.com";
+
   ngOnInit() {
   }
 
