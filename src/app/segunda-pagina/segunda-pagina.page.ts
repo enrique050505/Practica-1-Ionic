70aad516c561c129a58d3024175a87dbbb0c8041
enrique050505 <enriquemartinalbodo@gmail.com>
2024-10-06 02:27:30 +0200

    cambios hechos en la pagina Sobre Nosotros

--- src/app/segunda-pagina/segunda-pagina.page.ts
+++ src/app/segunda-pagina/segunda-pagina.page.ts
@@ -13,16 +13,16 @@ export class SegundaPaginaPage implements OnInit {
 
   imgLogo ="../../assets/logo.jpg";
 
-  descripcion: string = "Bienvenido a PlayVibe, tu tienda online de videojuegos. Aquí encontrarás los últimos videojuegos del mercado por precios asequibles y las últimas noticias sobre los videojuegos más populares. Fundada por Rubén y Enrique a principios de 2021, nuestra aplicación está diseñada para brindarte una experiencia de compra fácil y entretenida. Únete a nosotros y disfruta de un mundo lleno de diversión y aventuras digitales.";
+  descripcion: string = "¡Bienvenido a PlayVibe, tu tienda online de videojuegos! Aquí encontrarás los últimos videojuegos del mercado por precios asequibles, además de estar al tanto de las novedades y tendencias en el mundo de los videojuegos. Desde su lanzamiento a principios de 2021, nuestra plataforma está diseñada para brindarte una experiencia de compra intuitiva y entretenida. ¡¡Únete a nosotros, descubre y disfruta de un mundo lleno de diversión y aventuras digitales!!";
 
   estadisticas = [
-    {nombre: "Número Total de Videojuegos", valor:10, icono:"game-controller-outline"},
-    {nombre: "Usuarios Registrados", valor: 50, icono:"people-outline"},
-    {nombre: "Ventas Mensuales", valor: 1500, icono:"cash-outline"},
-    {nombre: "Reseñas Videojuegos",  valor: 4.6, icono:"star-outline"},
-    {nombre: "Promedio de Calificación de Videojuegos", valor:4.4, icono:"thumbs-up-outline"},
+    {nombre: "Total de Videojuegos", valor:10, icono:"game-controller-outline"},
+    {nombre: "Usuarios Registrados", valor: 350, icono:"people-outline"},
+    {nombre: "Ventas Mensuales", valor: 400, icono:"cash-outline"},
+    {nombre: "Reseñas Videojuegos",  valor: 4.4, icono:"star-outline"},
+    {nombre: "Valoración media", valor:4.6, icono:"thumbs-up-outline"},
     {nombre: "Juego Más Vendido", valor: "FC 25", icono:"trophy-outline"},
-    {nombre: "Premios Ganados por la Tienda", valor: 4, icono:"medal-outline"}
+    {nombre: "Premios Ganados", valor: 4, icono:"medal-outline"}
   ];
 
   ngOnInit() { 
