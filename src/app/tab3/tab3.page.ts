c2caee88c4414f1833283cc53f9b26343286bae1
Rubenchus18 <rubengarci327@gmail.com>
2024-10-10 13:56:44 +0200

    Trabajo finalizado, cambios en la pagina 3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -10,6 +10,6 @@ export class Tab3Page {
   constructor() {}
   iconoCasa = "home-outline";
   img_banne="https://th.bing.com/th/id/OIG1.Lv_ozM.zwcYxORMiK3o3?pid=ImgGn";
-  img_gmail="https://www.liveagent.com/app/uploads/2020/11/logo-gmail.png";
-  img_candado="https://cdn-icons-png.flaticon.com/512/6772/6772234.png";
+  img_gmail="mail-outline";
+  img_candado="lock-closed-outline";
 }
